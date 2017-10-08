0817ad4aaaba6d9e9c88bd808cc9c15866b18bed
Ivan Mitkov <ivan.mitkov@gmail.com>
2017-10-08 17:22:34 +0300

    Adding network connectivity

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,27 +15,83 @@
  */
 package com.example.android.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mWeatherDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
-        mWeatherDisplay = (TextView)findViewById(R.id.tv_weather_data);
-        String[] fakeWeather ={"7/10/2017 sunny 12C- 21C",
-                "8/10/2017 rain 2C- 12C",
-                "9/10/2017 fog 8C- 21C",
-                "10/10/2017 sunny 12C- 21C" };
-        for(String day:fakeWeather){
-            mWeatherDisplay.append(day+"\n\n\n");
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
+        loadWeatherData();
 
+    }
+
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    //sending location as string and getting array of string with weathe conditions
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected String[] doInBackground(String... params) {
+            /* If there's no zip code, there's nothing to look up. */
+            if (params.length == 0) {
+                return null;
+            }
+            String location = params[0];
+            //creating url for this location
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                //creating connection and getting response as json string
+                String jsonWeatherResponse =
+                        NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+                //parsing json string to create array of conditions
+                String[] simpleJsonWeatherData =
+                        OpenWeatherJsonUtils
+                                .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+                return simpleJsonWeatherData;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+
+        @Override
+        protected void onPostExecute(String[] weatherConditions) {
+
+            if (weatherConditions != null) {
+                for (String condition : weatherConditions) {
+                    mWeatherDisplay.append(condition + "\n\n\n");
+                }
+
+            }
         }
     }
 }
