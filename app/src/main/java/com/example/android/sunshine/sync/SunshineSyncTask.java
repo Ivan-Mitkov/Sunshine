f7dfd885712bb7621dc13d77ba7e4274b521a484
Ivan Mitkov <ivan.mitkov@gmail.com>
2017-11-19 15:50:53 +0200

    Add notifications

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -3,9 +3,12 @@ package com.example.android.sunshine.sync;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.text.format.DateUtils;
 
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.NotificationUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
@@ -31,6 +34,15 @@ public class SunshineSyncTask{
                 //   (4) If we have valid results, delete the old data and insert the new
                 contentResolver.delete(WeatherContract.WeatherEntry.CONTENT_URI,null,null);
                 contentResolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI,contentValues);
+            }
+            // (13) Check if notifications are enabled
+            boolean notificationEnabled = SunshinePreferences.areNotificationsEnabled(context);
+//               (14) Check if a day has passed since the last notification
+            long timePassed=SunshinePreferences.getEllapsedTimeSinceLastNotification(context);
+            boolean isDayPassed =(timePassed>= DateUtils.DAY_IN_MILLIS);
+//               (15) If more than a day have passed and notifications are enabled, notify the user
+            if(isDayPassed){
+                NotificationUtils.notifyUserOfNewWeather(context);
             }
 
         }
